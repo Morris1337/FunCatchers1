8a115eed32c283aa5b99d7ad578303aec942a37c
Morris1337 <117284517+Morris1337@users.noreply.github.com>
2023-03-05 01:31:02 +0200

    12

--- index.js
+++
@@ -1,7 +0,0 @@
-let mailBoxes = ["","","","","","",""]
-function deliverMessage (boxNum, message) {
-  if (mailBoxes[boxNum] != undefinded) {
-    mailBoxes[boxNum] = message;
-    console.log(mailBoxes);
-  }
- }
